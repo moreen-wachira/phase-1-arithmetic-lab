5e3870568e8e519c755d72279cd55ceb94890654
Moreen <moreencharles@gmail.com>
2023-10-05 01:22:36 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
+describe('multiply', function() {
+    it("is an equation whose multiplied results will equal 62", function() {
+      expect(var1).to.be.a('number')
+      expect(var2).to.be.a('number')
+      expect(multiply).to.eq(62)
+    });
+});
 // Write your code here
+describe('random', function() {
+    it("is generates a random integer greater than 0", function() {
+      expect(random).to.be.a('number')
+      expect(random).to.not.eq(0)
+    });
+  });
+
+  describe('mod', function() {
+    it("is generates a remainder that is equal to 4", function() {
+      expect(mod).to.eq(4)
+    });
+  });
+
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
